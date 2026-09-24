beb1929318cf45894f1aa8ad9138afdf380c85b0
Ludivine <Ludivinedjeni@gmail.com>
2026-09-23 23:12:15 -0400

    Affichage de la page equipe

--- Projet/Vues/equipes/index.php
+++ Projet/Vues/equipes/index.php
@@ -1,3 +1,8 @@
+<?php
+
+declare(strict_types=1);
+?>
+
 <h1>Les équipes</h1>
 
 <?php if (empty($equipes)): ?>
@@ -10,7 +15,7 @@
         <?php foreach ($equipes as $equipe): ?>
 
             <li>
-                <a href="index.php?action=equipe&id=<?= $equipe['id'] ?>">
+                <a href="index.php?action=equipe&id=<?= $equipe['idEquipe'] ?>">
                     <?= htmlspecialchars($equipe['nom'], ENT_QUOTES, 'UTF-8') ?>
                 </a>
             </li>
